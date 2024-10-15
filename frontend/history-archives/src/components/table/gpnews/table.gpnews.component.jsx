7c4540ae281cfffaf7036b51fc73c6851f69c82e
xavier evans <evans.xavier@ymail.com>
2024-10-15 12:28:18 -0400

    updated deployment script

--- frontend/history-archives/src/components/table/gpnews/table.gpnews.component.jsx
+++ frontend/history-archives/src/components/table/gpnews/table.gpnews.component.jsx
@@ -19,7 +19,7 @@ const TableGPnews = () => {
             setIsLoading(true);
             async function getIssues() {
                 try {
-                    const response = await axios.get(`http://10.1.5.203/api/gp_news/issues?publishYear=${currentYear}`)
+                    const response = await axios.get(`${process.env.REACT_APP_EXPRESS_SERVER}/gp_news/issues?publishYear=${currentYear}`)
                     const issues = response.data.data;
                     setNewspapers(issues);
                 } catch (error) {
