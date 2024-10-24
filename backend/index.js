2058ec68a7cb9970a6bb8d4ffb839dba68afa1cd
xavier evans <evans.xavier@ymail.com>
2024-10-24 10:41:49 -0400

    updated site tab logo

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ import GPreviewRouter from "./routes/gpreview.route.js";
 import GPcivicRouter from "./routes/gpcivic.route.js";
 import GPheritageRouter from "./routes/gpheritage.route.js";
 import GPmagazineRouter from "./routes/gpmagazine.route.js";
-import AdminUploadRouter from "./routes/upload.route.js";
+//import AdminUploadRouter from "./routes/upload.route.js";
 
 const app = express();
 // middleware for parsing request body
@@ -27,10 +27,10 @@ origin: process.env.INTERNAL_SERVER, // until front end has domain name, local h
 optionsSuccessStatus: 200,
 };
 
-const corsOptions_admin = {
+/*const corsOptions_admin = {
     origin: process.env.ADMIN_APP, // until front end has domain name, local host is used.// set to 3000 for testing
     optionsSuccessStatus: 200,
-};
+};*/
 
 app.use(cors(corsOptions));
 
@@ -49,7 +49,7 @@ app.use('/gp_civic', cors(corsOptions), GPcivicRouter);
 app.use('/gp_heritage', cors(corsOptions), GPheritageRouter);
 app.use('/gp_magazine', cors(corsOptions), GPmagazineRouter);
 
-app.use('/upload', /*cors(corsOptions_admin),*/ AdminUploadRouter); // user app shall not interface with this route.
+//app.use('/upload', /*cors(corsOptions_admin),*/ AdminUploadRouter); // user app shall not interface with this route.
 
 
 
